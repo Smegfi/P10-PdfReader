a57cdb5a92c705127a223708e71229b7c2f28af0
Šmegfi <feedopeedo@gmail.com>
2023-01-30 15:52:43 +0100

    update

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
+import configparser
 import zalohovac
 import application
 from datetime import datetime
 
+cfg = configparser.ConfigParser()
+cfg.read("config.ini", "UTF-8")
+runPremistovac = cfg["DEFAULT"]["Premistovac"]
+runZalohovac = cfg["DEFAULT"]["Zalohovac"]
+
 start = datetime.now()
-zalohovac.MoveFiles()
-zalohovac.CleanBackup()
+
+if runPremistovac == 1:
+    zalohovac.MoveFiles()
+if runZalohovac == 1:
+    zalohovac.CleanBackup()
 
 application.ConvertToImage()
 
